b12315440eab3601c9e79eca769e50690115cdb6
Pardus26 <yamak.tamer@gmail.com>
2026-08-01 08:49:27 +0300

    Create TamerCAD/core/feature/src/main/java/com/tamercad/core/feature/FeatureType.kt

---
+++ TamerCAD/core/feature/src/main/java/com/tamercad/core/feature/FeatureType.kt
@@ -0,0 +1,49 @@
+package com.tamercad.core.feature
+
+
+/**
+ * Supported parametric feature types.
+ */
+enum class FeatureType {
+
+
+    Sketch,
+
+
+    Extrude,
+
+
+    Revolve,
+
+
+    Sweep,
+
+
+    Loft,
+
+
+    Fillet,
+
+
+    Chamfer,
+
+
+    Mirror,
+
+
+    Pattern,
+
+
+    Shell,
+
+
+    BooleanUnion,
+
+
+    BooleanDifference,
+
+
+    BooleanIntersection
+
+
+}
