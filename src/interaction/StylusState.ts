783f833fc51b68355bea1d148c4c011070e69f39
Pardus26 <yamak.tamer@gmail.com>
2026-08-04 16:17:27 +0300

    Create src/interaction/StylusState.ts

---
+++ src/interaction/StylusState.ts
@@ -0,0 +1,29 @@
+export interface StylusState {
+
+
+    x:number;
+
+
+    y:number;
+
+
+    pressure:number;
+
+
+    tiltX:number;
+
+
+    tiltY:number;
+
+
+    buttons:number;
+
+
+    hovering:boolean;
+
+
+    down:boolean;
+
+
+
+}
