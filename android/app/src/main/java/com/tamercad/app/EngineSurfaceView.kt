d0f969acad30207bbdd815985e39ee6870ce26bd
Pardus26 <yamak.tamer@gmail.com>
2026-08-04 14:41:42 +0300

    Update EngineSurfaceView.kt

--- android/app/src/main/java/com/tamercad/app/EngineSurfaceView.kt
+++ android/app/src/main/java/com/tamercad/app/EngineSurfaceView.kt
@@ -1,68 +1,316 @@
 package com.tamercad.app
 
+
 import android.content.Context
 import android.opengl.GLSurfaceView
 import android.view.MotionEvent
 
+
+
 class EngineSurfaceView(
+
     context: Context
+
 ) : GLSurfaceView(context) {
+
+
 
     private val renderer: TamerCADRenderer
 
+
+
+
+
     init {
+
+
 
         /*
             OpenGL ES 3.0
          */
         setEGLContextClientVersion(3)
 
-        renderer = TamerCADRenderer()
 
-        setRenderer(renderer)
 
-        renderMode = RENDERMODE_CONTINUOUSLY
+        renderer =
+            TamerCADRenderer()
 
-        preserveEGLContextOnPause = true
+
+
+        setRenderer(
+            renderer
+        )
+
+
+
+        renderMode =
+            RENDERMODE_CONTINUOUSLY
+
+
+
+        preserveEGLContextOnPause =
+            true
+
+
     }
 
+
+
+
+
+
+
+
+
     override fun onTouchEvent(
+
         event: MotionEvent
+
     ): Boolean {
 
-        when (event.actionMasked) {
 
-            MotionEvent.ACTION_DOWN -> {
 
-                renderer.touchDown(
-                    event.x,
-                    event.y
+        val pointerIndex =
+
+            event.actionIndex
+
+
+
+
+
+        val pointerId =
+
+            event.getPointerId(
+                pointerIndex
+            )
+
+
+
+
+
+        val x =
+
+            event.getX(
+                pointerIndex
+            )
+
+
+
+
+
+        val y =
+
+            event.getY(
+                pointerIndex
+            )
+
+
+
+
+
+        val pressure =
+
+            event.getPressure(
+                pointerIndex
+            )
+
+
+
+
+
+
+
+
+
+        val toolType =
+
+            event.getToolType(
+                pointerIndex
+            )
+
+
+
+
+
+
+
+
+
+        when(event.actionMasked) {
+
+
+
+            MotionEvent.ACTION_DOWN,
+
+            MotionEvent.ACTION_POINTER_DOWN -> {
+
+
+
+                EngineBridge.pointerDown(
+
+                    pointerId,
+
+                    x,
+
+                    y,
+
+                    pressure
+
                 )
+
+
             }
+
+
+
+
+
+
+
+
 
             MotionEvent.ACTION_MOVE -> {
 
-                renderer.touchMove(
-                    event.x,
-                    event.y
-                )
+
+
+                /*
+                    Multi-touch desteği
+
+                    tüm aktif pointerları
+                    gönderiyoruz
+                 */
+
+                for(
+                    i in 0 until event.pointerCount
+                ){
+
+
+                    EngineBridge.pointerMove(
+
+                        event.getPointerId(i),
+
+                        event.getX(i),
+
+                        event.getY(i),
+
+                        event.getPressure(i)
+
+                    )
+
+
+                }
+
+
             }
 
-            MotionEvent.ACTION_UP -> {
 
-                renderer.touchUp(
-                    event.x,
-                    event.y
+
+
+
+
+
+
+
+            MotionEvent.ACTION_UP,
+
+            MotionEvent.ACTION_POINTER_UP -> {
+
+
+
+                EngineBridge.pointerUp(
+
+                    pointerId,
+
+                    x,
+
+                    y
+
                 )
+
+
             }
+
+
+
+
+
+
+
+
+
+            MotionEvent.ACTION_CANCEL -> {
+
+
+
+                EngineBridge.pointerUp(
+
+                    pointerId,
+
+                    x,
+
+                    y
+
+                )
+
+
+            }
+
+
         }
 
+
+
+
+
         return true
+
     }
+
+
+
+
+
+
+
+
+
+    private fun isStylus(
+
+        event:MotionEvent
+
+    ):Boolean {
+
+
+        return event.getToolType(
+
+            event.actionIndex
+
+        ) ==
+
+        MotionEvent.TOOL_TYPE_STYLUS
+
+
+
+    }
+
+
+
+
+
+
+
+
 
     fun shutdown() {
 
+
+
         renderer.shutdown()
 
+
+
     }
+
+
+
 }
